2b56ee0f9a4f54bec1c4cff545ed284b7ff15ae6
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-25 23:05:35 +0900

    Add files via upload

---
+++ JavaScript_practiceI_conditional branch_booleans and comparison operators2.js
@@ -0,0 +1,11 @@
+const password = "1234567";
+
+// If the value of password is "1234567", output "Login succeeded!".
+if(password === "1234567"){
+  console.log("Login successful!") ;
+}
+
+// If the value of password is not "1234567", output "The password you entered is incorrect!".
+if(password !== "1234567"){
+  console.log("The password you entered is incorrect!") ;
+}
